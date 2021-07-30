0b13e2819d1600c51421dc1ab452d9dcc689c75a
Yashwanth Ravipati <yashwanth.ravipati@icloud.com>
2021-07-30 10:48:08 -0500

    Initial Commit

---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Yashwanth Ravipati on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
